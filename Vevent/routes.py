3208377cab3592abd583d827ce5033fcbc012670
musaasad2020 <musaasad.2020@gmail.com>
2022-05-20 16:24:58 -0400

    SETUP

---
+++ Vevent/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, request, jsonify, make_response
+import json
+
+from Vevent import app, db
+from Vevent.models import element
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if request.method == "GET":
+        return make_response(jsonify([elmnt.value for elmnt in element.query.all()]), 200)
+    else:
+        db.session.add(element(json.loads(request.data)["task"]))
+        db.session.commit()
+        print([elmnt.value for elmnt in element.query.all()])
+        return make_response(jsonify([elmnt.value for elmnt in element.query.all()]), 200)
+
+
+@app.route("/<id>", methods=["DELETE", "PUT"])
+def task(id):
+    if request.method == "DELETE":
+        element.query.filter_by(_id=id).delete()
+        db.session.query(element).filter(element._id > id).update(
+            {element._id: element._id - 1})
+        db.session.commit()
+        return make_response(jsonify([elmnt.value for elmnt in element.query.all()]), 200)
+    else:
+        db.session.query(element).filter(element._id == id).update(
+            {element.value: json.loads(request.data)["value"]})
+        db.session.commit()
+        return make_response(jsonify([elmnt.value for elmnt in element.query.all()]), 200)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Vevent',
+    packages=['Vevent'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
