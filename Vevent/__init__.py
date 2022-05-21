461f0359509379a0bb163b7b03377afc56f02069
musaasad2020 <musaasad.2020@gmail.com>
2022-05-20 22:49:59 -0400

    Back-end updates

--- Vevent/__init__.py
+++ Vevent/__init__.py
@@ -1,13 +1,17 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_googlemaps import GoogleMaps
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['GOOGLEMAPS_KEY'] = os.environ['GOOGLEMAPS_KEY']
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+GoogleMaps(app)
 
 import Vevent.routes
--- Vevent/routes.py
+++ Vevent/routes.py
@@ -1,4 +1,5 @@
-from flask import jsonify, redirect, render_template, request, session, url_for, flash
+from flask import redirect, render_template, request, session, url_for, flash
+from flask_googlemaps import Map
 
 from Vevent import app, db
 from Vevent.models import User, Event
@@ -9,7 +10,7 @@ def login():
     session['user'] = None
     return render_template('login.html')
 
-@app.route("/events", methods=["GET", "POST"]) # check if authenticated
+@app.route("/events", methods=["GET", "POST"])
 def events():
     if request.method == "GET":
         flash("Not authenticated.")
@@ -22,7 +23,10 @@ def events():
     user = load_user({"email": email, "password": password})
     if user:
         session['user'] = email
-        return render_template('events.html', events=[{"value": event.value, "id": event._id} for event in Event.query.all()])
+        return render_template('events.html',
+            events=[{"value": event.value, "id": event._id} for event in Event.query.all()],
+            map=Map(identifier="Event_Map", lat=40, lng=-75)
+            )
     elif not User.query.filter_by(email=email).first():
         new_user = User(
             email = email,
@@ -35,21 +39,21 @@ def events():
     flash("Incorrect information.")
     return redirect(url_for('login'))
 
-@app.route("/events/<id>") # check if authenticated
+@app.route("/events/<id>")
 def event(id):
     if not session['user']:
         flash("Not authenticated.")
         return redirect(url_for('login'))
     return render_template('event.html')
 
-@app.route("/create") # check if authenticated
+@app.route("/create") # make sure all fields are valid before committing to db
 def create():
     if not session['user']:
         flash("Not authenticated.")
         return redirect(url_for('login'))
     return render_template('create.html')
 
-@app.route("/faq") # check if authenticated
+@app.route("/faq")
 def faq():
     if not session['user']:
         flash("Not authenticated.")
