86b60447bfb663464bfa47bc71639841b1e2e9f9
musaasad2020 <musaasad.2020@gmail.com>
2022-05-20 20:35:04 -0400

    Updates

--- Vevent/models.py
+++ Vevent/models.py
@@ -1,10 +1,11 @@
-from Vevent import db
 from flask_login import UserMixin
+
+from Vevent import db
 
 class User(UserMixin, db.Model):
     email = db.Column(db.String, primary_key=True)
     password = db.Column(db.String)
-    
+
     def __init__(self, email, password):
         self.email = email
         self.password = password
@@ -12,9 +13,13 @@ class User(UserMixin, db.Model):
 class Event(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.String)
+    email = db.Column(db.String)
+    location = db.Column(db.String)
 
-    def __init__(self, value):
+    def __init__(self, value, email, location):
         self.value = value
+        self.email = email
+        self.location = location
 
 db.create_all()
     
--- Vevent/routes.py
+++ Vevent/routes.py
@@ -1,38 +1,57 @@
-from flask import redirect, render_template, request, session, url_for
+from flask import jsonify, redirect, render_template, request, session, url_for, flash
 
 from Vevent import app, db
-from Vevent.models import User
+from Vevent.models import User, Event
 from Vevent.utils import load_user
 
 @app.route("/")
 def login():
+    session['user'] = None
     return render_template('login.html')
 
-@app.route("/events", methods=["POST"]) # check if authenticated
+@app.route("/events", methods=["GET", "POST"]) # check if authenticated
 def events():
+    if request.method == "GET":
+        flash("Not authenticated.")
+        return redirect(url_for('login'))
     email = request.form['email']
     password = request.form['password']
+    if not email or not password:
+        flash("Please provide data.")
+        return redirect(url_for('login'))
     user = load_user({"email": email, "password": password})
     if user:
-        print(session)
-        return render_template('events.html')
+        session['user'] = email
+        return render_template('events.html', events=[{"value": event.value, "id": event._id} for event in Event.query.all()])
     elif not User.query.filter_by(email=email).first():
         new_user = User(
             email = email,
             password = password
         )
         db.session.add(new_user)
-        print(new_user.email, new_user.password)
+        db.session.commit()
+        flash("Account created!")
+        return redirect(url_for('login'))
+    flash("Incorrect information.")
     return redirect(url_for('login'))
 
 @app.route("/events/<id>") # check if authenticated
 def event(id):
+    if not session['user']:
+        flash("Not authenticated.")
+        return redirect(url_for('login'))
     return render_template('event.html')
 
 @app.route("/create") # check if authenticated
 def create():
+    if not session['user']:
+        flash("Not authenticated.")
+        return redirect(url_for('login'))
     return render_template('create.html')
 
 @app.route("/faq") # check if authenticated
 def faq():
+    if not session['user']:
+        flash("Not authenticated.")
+        return redirect(url_for('login'))
     return render_template('faq.html')
